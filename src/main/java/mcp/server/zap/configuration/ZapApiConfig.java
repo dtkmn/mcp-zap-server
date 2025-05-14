effe0a63732fa1e22a65c4a2f2607e3dad2193f8
Daniel Tse <danieltse@gmail.com>
2025-05-14 17:55:14 +1000

    ZapApiConfig created

---
+++ src/main/java/mcp/server/zap/configuration/ZapApiConfig.java
@@ -0,0 +1,20 @@
+package mcp.server.zap.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zaproxy.clientapi.core.ClientApi;
+
+@Configuration
+public class ZapApiConfig {
+
+    @Bean
+    public ClientApi zapClientApi(
+            @Value("${zap.server.url:localhost}") String zapApiUrl,
+            @Value("${zap.server.port:8090}") int zapApiPort,
+            @Value("${zap.server.apiKey:}") String zapApiKey
+    ) {
+        return new ClientApi(zapApiUrl, zapApiPort, zapApiKey);
+    }
+
+}
